50cd5b7d299bc2c9627485241965024d2bd6bf5d
403room-15 <403room-15@DESKTOP-SF2NH8B>
2018-04-27 14:06:05 +0900

    0427(kim)

--- cody/src/main/java/com/bitcamp/cody/controller/CodyController.java
+++ cody/src/main/java/com/bitcamp/cody/controller/CodyController.java
@@ -18,43 +18,42 @@ import com.bitcamp.cody.service.CodyService;
 @RequestMapping("/codyForm")
 public class CodyController {
 
-		@Autowired
-		CodyService service;
-		
-		@RequestMapping(method=RequestMethod.GET)
-		public String codyForm() {
-			return "cody/codyForm";
-		}
+	@Autowired
+	CodyService service;
 
-		@RequestMapping(method=RequestMethod.POST)
-		public String codyInsert(CodyDto cody, Model model, HttpServletRequest request ) 
-				throws IllegalStateException, IOException {
-			// 서비스에  회원 데이터 저장 요청
-			// 결과 : 1 / 0
-			// model 에 저장
-			System.out.println(cody);
-			
-			int resultCnt = service.codyInsert(cody, request);
-			
-			String msg = "정상적으로 등록되었습니다.";
-			   
-		
-			if(resultCnt == 0)
-				msg = "등록이 되지 않았습니다. 담당자에게 문의 해주세요.";		
-			
-			model.addAttribute("msg", msg);
-			
-			return "cody/codyFormOk";
+	@RequestMapping(method = RequestMethod.GET)
+	public String codyForm() {
+		return "cody/codyForm";
 	}
-		@ExceptionHandler(IOException.class)
-		public String ex1(IOException e) {
-			return "error/ex1";
-		}
-		
-		@ExceptionHandler(IllegalStateException.class)
-		public String ex2(IllegalStateException e) {
-			return "error/ex2";
-		}
-		
-}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public String codyInsert(CodyDto cody, Model model, HttpServletRequest request)
+			throws IllegalStateException, IOException {
+		// 서비스에 회원 데이터 저장 요청
+		// 결과 : 1 / 0
+		// model 에 저장
+		System.out.println(cody);
+
+		int resultCnt = service.codyInsert(cody, request);
+
+		String msg = "정상적으로 등록되었습니다.";
+
+		if (resultCnt == 0)
+			msg = "등록이 되지 않았습니다. 담당자에게 문의 해주세요.";
+
+		model.addAttribute("msg", msg);
+
+		return "cody/codyFormOk";
+	}
+
+	@ExceptionHandler(IOException.class)
+	public String ex1(IOException e) {
+		return "error/ex1";
+	}
+
+	@ExceptionHandler(IllegalStateException.class)
+	public String ex2(IllegalStateException e) {
+		return "error/ex2";
+	}
+
+}
