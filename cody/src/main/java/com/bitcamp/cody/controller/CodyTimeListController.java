09b0aefc83d5c155666d6c7e9bbf4b59639e611b
room403 28 <room403 28@room403-28>
2018-05-10 16:22:09 +0900

    Lim0510

--- cody/src/main/java/com/bitcamp/cody/controller/CodyTimeListController.java
+++ cody/src/main/java/com/bitcamp/cody/controller/CodyTimeListController.java
@@ -1,6 +1,8 @@
 package com.bitcamp.cody.controller;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,8 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bitcamp.cody.dto.CodyDto;
+import com.bitcamp.cody.service.CodyItemListService;
 import com.bitcamp.cody.service.CodyTimeListService;
-
 
 @Controller
 public class CodyTimeListController {
@@ -17,19 +19,29 @@ public class CodyTimeListController {
 	@Autowired
 	CodyTimeListService codyTimeListService;
 
-	/*-------------------------------- 리스트 ------------------------------*/
-	@RequestMapping("/codyTimeList")
-	public String codyTimeList(Model model) {
+	@Autowired
+	CodyItemListService codyItemListService;
+
+	/* -------------------------------- 리스트 ------------------------------ */
+	@RequestMapping("/codyTimeList")	
+	
+	public String codyTimeList(Model model){
 
 		List<CodyDto> codytimes = codyTimeListService.getCodyTimeList();
 		
+		Map<Integer, Object> map = new HashMap<Integer,Object>();
 		
+		for(int i= 0; i<codytimes.size(); i++) {
+			
+		  map.put(codytimes.get(i).getCody_idx(), codyItemListService.getCodyItemList(codytimes.get(i).getCody_idx()));	  
+		}	
 		System.out.println("전체리스트 : " + codytimes.toString());
+		System.out.println("map" + map.toString());
 		
-		model.addAttribute("codytimes", codytimes);
-
+		model.addAttribute("codytimes",codytimes);
+		model.addAttribute("map",map);
+		 
 		return "time/codyTimeList";
-
 	}
 }
-  
+ 
--- cody/src/main/java/com/bitcamp/cody/dao/CodyTimeListDao.java
+++ cody/src/main/java/com/bitcamp/cody/dao/CodyTimeListDao.java
@@ -3,13 +3,17 @@ package com.bitcamp.cody.dao;
 import java.util.List;
 
 import com.bitcamp.cody.dto.CodyDto;
+import com.bitcamp.cody.dto.ItemDto;
 
 public interface CodyTimeListDao {
 	
 	public List<CodyDto> selectList();
+	public List<ItemDto> selectList1(int cody_idx);
+
+	
 	
 	 
-	
+	 
 	
 
 }
