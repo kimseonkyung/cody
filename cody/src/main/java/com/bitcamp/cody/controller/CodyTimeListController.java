5ab93f07ebd6c5c96e8e3f0f75d837e5ff484bcd
room403 28 <room403 28@room403-28>
2018-06-04 18:01:09 +0900

    0604_타임라인 회원정보 출력 추가

--- cody/src/main/java/com/bitcamp/cody/controller/CodyTimeListController.java
+++ cody/src/main/java/com/bitcamp/cody/controller/CodyTimeListController.java
@@ -11,15 +11,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.bitcamp.cody.dto.CodyDto;
 import com.bitcamp.cody.dto.ItemDto;
+import com.bitcamp.cody.dto.MemberDto;
 import com.bitcamp.cody.service.CodyItemListService;
 import com.bitcamp.cody.service.CodyTimeListService;
+import com.bitcamp.cody.service.RankingSerivce;
 
 @Controller
 public class CodyTimeListController {
 
+	
+	@Autowired
+	RankingSerivce rankingSerivce;
 	@Autowired
 	CodyTimeListService codyTimeListService;
-
 	@Autowired
 	CodyItemListService codyItemListService;
 
@@ -32,13 +36,16 @@ public class CodyTimeListController {
 		ArrayList<HashMap<Object, Object>> irr = new ArrayList<>();
 
 		for (CodyDto time : codytimes) {	
-			
+			MemberDto member = rankingSerivce.getId(time.getMember_idx());		
 			HashMap<Object, Object> map = new HashMap<>();
 			List<ItemDto> itemtime = new ArrayList<>();		
 			itemtime = codyItemListService.getCodyItemList(time.getCody_idx());
 			map.put(time.getCody_idx(),itemtime);	
 			map.put("codytitle",time.getCody_title());
 			map.put("codyimage",time.getCody_image());
+			map.put("id", member.getMember_id() );
+			map.put("photo", member.getMember_photo() );
+			map.put(time, itemtime);
 			irr.add(map);		
 		}
 		System.out.println("irr : " + irr.toString());
